97942e3d747b430f4f9cbff1380a6eb5e7e77af5
hyun2y00 <pottq577@naver.com>
2025-05-20 13:06:56 +0900

    feat(search): 정적 팩토리 메서드를 활용하여 객체 인스턴스화 코드 추가

--- src/main/java/com/example/springrider/domain/search/entity/Search.java
+++ src/main/java/com/example/springrider/domain/search/entity/Search.java
@@ -9,14 +9,18 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
+@Builder
 @Entity
 @Getter
 @Table(name = "search")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Search {
 
     @Id
@@ -33,4 +37,21 @@ public class Search {
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
+
+    public static Search of(String keyword){
+//        return new Search(keyword);
+        return Search.builder()
+            .keyword(keyword)
+            .createdAt(LocalDateTime.now())
+            .build();
+    }
+
+    public static Search of(String keyword, Integer count) {
+//        return new Search(keyword, count);
+        return Search.builder()
+            .keyword(keyword)
+            .count(count)
+            .createdAt(LocalDateTime.now())
+            .build();
+    }
 }
