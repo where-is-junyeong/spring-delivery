43ac8e8c2108b9f5c4c2169057a72d49be58235d
hyun2y00 <pottq577@naver.com>
2025-05-20 19:42:20 +0900

    feat(search): CaffeineCacheManager 빈과 설정 파일에 등록

--- src/main/java/com/example/springrider/config/CacheConfig.java
+++ src/main/java/com/example/springrider/config/CacheConfig.java
@@ -1,10 +1,26 @@
 package com.example.springrider.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(Caffeine.newBuilder()
+            .expireAfterWrite(10, TimeUnit.HOURS)
+            .maximumSize(1000)
+        );
+
+        return caffeineCacheManager;
+    }
 
 }
